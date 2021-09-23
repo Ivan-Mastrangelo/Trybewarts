0649415bee64165dbd066f8af75d3caae9e5ef18
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-09-23 18:51:58 -0300

    Add requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputSenha = document.querySelector('#password');
 const buttonEntrar = document.querySelector('#btn-entrar');
 const buttonEnviar = document.querySelector('#submit-btn');
 const inputAgreement = document.querySelector('#agreement');
+const textArea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
 
 function login() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -23,3 +25,7 @@ function sendForm() {
 }
 
 inputAgreement.addEventListener('click', sendForm);
+
+textArea.addEventListener('keyup', () => {
+  counter.innerHTML = 500 - textArea.value.length;
+});
