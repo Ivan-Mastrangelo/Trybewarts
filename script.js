ec9beacc237939e485d3400dd1f4889b78f731e8
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-09-23 10:52:23 -0300

    Add requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputEmail = document.querySelector('#email');
 const inputSenha = document.querySelector('#password');
 const buttonEntrar = document.querySelector('#btn-entrar');
+const buttonEnviar = document.querySelector('#submit-btn');
+const inputAgreement = document.querySelector('#agreement');
 
 function login() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -11,3 +13,13 @@ function login() {
 }
 
 buttonEntrar.addEventListener('click', login);
+
+function sendForm() {
+  if (buttonEnviar.disabled === true) {
+    buttonEnviar.disabled = false;
+  } else {
+    buttonEnviar.disabled = true;
+  }
+}
+
+inputAgreement.addEventListener('click', sendForm);
